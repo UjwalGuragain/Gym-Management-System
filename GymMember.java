cfa9a4e76dd01791518b725de3d355abe5354837
Ujwal-Guragain <ujwalguragain436@gmail.com>
2026-04-04 22:58:55 +0545

    Create GymMember abstract class with attributes
    Added attributes and constructor for GymMember class.

--- GymMember.java
+++ GymMember.java
@@ -1,1 +1,27 @@
+//An abstract parent class GymMember is created
+public abstract class GymMember {
+    protected int id;
+    protected String name;
+    protected String location;
+    protected String phone;
+    protected String email;
+    protected String gender;
+    protected String DOB;
+    protected String membershipStartDate;
+    protected int attendance = 0;
+    protected double loyaltyPoints = 0.0;
+    protected boolean activeStatus = false;
+    protected boolean isReverted;
+
+    //A constructor that accepts the various details of the member as parameters
+    public GymMember(int id, String name, String location, String phone, String email, String gender, String DOB, String membershipStartDate) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.phone = phone;
+        this.email = email;
+        this.gender = gender;
+        this.DOB = DOB;
+        this.membershipStartDate = membershipStartDate;
+    }
 
