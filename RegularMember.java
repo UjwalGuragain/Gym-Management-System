072deb9e0983bc2475ab7eae89b3912dc2a1414b
Ujwal-Guragain <ujwalguragain436@gmail.com>
2026-04-05 12:52:50 +0545

    Implement RegularMember class with attendance tracking
    Added RegularMember class that extends GymMember with attendance tracking and upgrade eligibility.

--- RegularMember.java
+++ RegularMember.java
@@ -1,1 +1,28 @@
-
+// A class that inherits GymMember class
+public class RegularMember extends GymMember{
+    private final int attendanceLimit = 30;
+    private boolean isEligibleForUpgrade = false;
+    private String removalReason = "";
+    private String referralSource;
+    private String plan = "Basic";
+    private double price = 6500.0;
+    private double paidAmount = 0.0; 
+    
+    //A constructor that accepts parameters for regular members and calls the parent constructor
+    public RegularMember(int id, String name, String location, String phone, String email, String gender, String DOB, String membershipStartDate, String referralSource) {
+        super(id, name, location, phone, email, gender, DOB, membershipStartDate);
+        this.referralSource = referralSource;
+    }
+    //Implementing the abstract method from parent class to track attendance and loyalty points of regular members
+    @Override
+    public void markAttendance() {
+        if (activeStatus) {
+            attendance++;
+            loyaltyPoints += 5;
+            if (attendanceLimit <= attendance) 
+            isEligibleForUpgrade = true;
+        } else 
+        {
+            System.out.println("Membership is not active, Activate first.");
+        }
+    }
