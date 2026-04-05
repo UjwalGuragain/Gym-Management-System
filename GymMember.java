92db62c4dc5edb429b20ec650df07a0f08e81de0
Ujwal-Guragain <ujwalguragain436@gmail.com>
2026-04-05 12:49:19 +0545

    Implement membership activation, deactivation, display methods, etc.
    Added methods for membership management and member details.

--- GymMember.java
+++ GymMember.java
@@ -25,3 +25,102 @@ public abstract class GymMember {
         this.membershipStartDate = membershipStartDate;
     }
 
+    //An abstract method is created to track the attendance and loyalty points of member and is to be implemented by subclasses
+    public abstract void markAttendance();
+
+    //A method that activates the membership
+    public void activateMembership()
+    {
+        activeStatus = true;
+    }
+
+    //A method that deactivates the membership
+    public void deactivateMembership()
+    {
+        if (activeStatus) {
+            activeStatus = false;
+        }
+        else {
+            System.out.println("Membership has already been deactivated.");
+        }
+    }
+
+    //A method to reset the member's attendance, loyalty points and active status
+    public void resetMember()
+    {
+        this.activeStatus = false;
+        this.loyaltyPoints = 0.0;
+        this.attendance = 0;
+    }
+
+    // Mark the member as reverted
+    public void markAsReverted() {
+        this.isReverted = true;  // Set the flag to true when reverted
+    }
+
+    // Check if the member is reverted
+    public boolean marksAsReverted() {
+        return this.isReverted;
+    }
+
+    //A method that displays the details of the member
+    public void display()
+    {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Location: " + location);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("Gender: " + gender);
+        System.out.println("DOB: " + DOB);
+        System.out.println("Membership Start Date: " + membershipStartDate);
+        System.out.println("Attendance: " + attendance);
+        System.out.println("Loyalty Points: " + loyaltyPoints);
+        System.out.println("Active Status: " + activeStatus);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getMembershipStartDate() {
+        return membershipStartDate;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public double getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
+}
+
