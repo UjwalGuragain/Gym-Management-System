b32f33c93f46fe9d2e2ffea036abf5eca361b0d0
Ujwal-Guragain <ujwalguragain436@gmail.com>
2026-04-28 23:56:09 +0545

    Implement basic Gym Management GUI layout

--- GymGUI.java
+++ GymGUI.java
@@ -1,1 +1,65 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.BorderFactory;
 
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
+
+public class GymGUI implements ActionListener {
+    // Components
+    public JFrame frame;
+    public JTextField fieldId, fieldName, fieldLocation, fieldPhone, fieldEmail, fieldDOB, fieldMembershipStart,
+            fieldReferralSource, fieldPaidAmount, fieldRemovalReason, fieldTrainerName;
+    public JRadioButton maleRadio, femaleRadio;
+    public JComboBox<String> DOB_Year, DOB_Month, DOB_Day;
+    public JComboBox<String> startyear, startmonth, startdate;
+    public JComboBox<String> membershipTypeBox, planForRegularBox;
+    public JTextField priceField;
+    public JButton btnAddRegular, btnAddPremium, btnDisplay, btnActivate, btnDeactivate,
+            btnMarkAttendance, btnUpgradePlan, btnCalculateDiscount, btnRevertRegular,
+            btnRevertPremium, btnPayDue, btnClear, btnSaveToFile, btnReadFromFile;
+    public ButtonGroup genderGroup;
+    
+        // Member List
+    private ArrayList<GymMember> memberList = new ArrayList<>();
+    public static void main(String[] args) {new GymGUI();}
+
+    public GymGUI() {
+        frame = new JFrame();
+        frame.setTitle("Gym Management System");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 800);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(Color.CYAN);
+        
+
+        JLabel titleLabel = new JLabel("Gym Management System", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        titleLabel.setBounds(200, 0, 900, 50);
+        frame.add(titleLabel);
+
+        create();
+        frame.setVisible(true);
+    }
+  
+}
